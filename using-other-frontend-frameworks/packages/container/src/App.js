e21abca74c48df783951c03e87bd87584a5df80f
Vladimir Stoyanov <vladsto101@gmail.com>
2023-12-30 23:33:28 +0200

    Added vue sub-project for the dashboard and configured it using webpack.

---
+++ using-other-frontend-frameworks/packages/container/src/App.js
@@ -0,0 +1,31 @@
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+import Progress from './components/Progress';
+import Header from './components/Header';
+
+const AuthLazy = lazy(() => import('./components/AuthApp'));
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+});
+
+export default () => {
+    return (
+        <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <Suspense fallback={<Progress />}>
+                        <Switch>
+                            <Route path='/auth' component={AuthLazy} />
+                            <Route path='/' component={MarketingLazy} />
+                        </Switch>
+                    </Suspense>
+                </div>
+            </StylesProvider>
+        </BrowserRouter>
+    );
+};
